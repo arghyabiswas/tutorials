deb673c405387e1f29bb63d0af37f7f848dfc694
Ashirwad Satapathi <ashirwadsatapathi.aim@gmail.com>
2019-07-24 18:27:54 +0530

    Solution for Assignment2B

---
+++ Assignment/Assignment2B/EmployeeModel/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeModel
+{
+    /// <summary>
+    /// Employee class inherit from IEmployee interface
+    /// </summary>
+    public class Employee : IEmployee
+    {
+        /// <summary>
+        /// Creating a constructor of Employee Class and accepting parameter employeeid of int data types
+        /// </summary>
+        /// <param name="employeeid"></param>
+        public Employee(int employeeid)
+        {
+            this.EmployeeId = employeeid;
+        }
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public int EmployeeId
+        {
+            get;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public IEmployee ReportingManager
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Assignment/Assignment2B/EmployeeModel/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeModel
+{
+    public class Manager : Employee, IManager
+    {
+        /// <summary>
+        /// Create a constructor of Manager class 
+        /// </summary>
+        /// <param name="employeeid"></param>
+        public Manager(int employeeid) : base(employeeid)
+        {
+
+        }
+        public IEmployee[] subordinates { get; set; }
+    }
+}
