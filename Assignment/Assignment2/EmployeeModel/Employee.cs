2344fabfe929297ddf2e09bbac7d256cb846e604
Ashirwad Satapathi <ashirwadsatapathi.aim@gmail.com>
2019-07-24 10:36:28 +0530

    Uploading assignment 2 dated 23/07/2019

---
+++ Assignment/Assignment2/EmployeeModel/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeModel
+{
+    public class Employee : IEmployee
+    {
+//        private int id;
+
+        public Employee(int Id)
+        {
+            this.Id = Id;
+        }
+        public string Email
+        {
+            get;
+
+            set;
+        }
+
+        public int Id
+        {
+            get;
+
+        }
+
+        public IEmployee Manager
+        {
+            get;
+
+            set;
+        }
+
+        public string Name
+        {
+            get;
+
+            set;
+        }
+    }
+}
+    
+    
+        
