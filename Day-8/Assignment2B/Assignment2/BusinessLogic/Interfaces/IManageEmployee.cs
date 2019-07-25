d90613421f6bcf92516989c47c6751a3be0abfa1
nbeura <52789961+nbeura@users.noreply.github.com>
2019-07-25 13:45:35 +0530

    Added Assignmnet-2B to Day-8

---
+++ Day-8/Assignment2B/Assignment2/BusinessLogic/Interfaces/IManageEmployee.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeModel.BusinessLogic.Interfaces;
+using System.Collections.Generic;
+
+namespace Assignment2.BusinessLogic.Interfaces
+{
+    public interface IManageEmployee
+    {
+        /// <summary>
+        /// Prints Total Number of Employee Present in the Organization
+        /// </summary>
+        /// <param name="employee">List of Employee</param>
+        void GetTotalEmployee(List<IEmployee> employee);
+
+        /// <summary>
+        /// Prints the Employee Details under a Manager, Uses Expression Mathod
+        /// </summary>
+        /// <param name="employee">List of Employee</param>
+        /// <param name="employeeId">ManagerId</param>
+        void GetSubOrdinates(List<IEmployee> employee, int employeeId);
+
+        /// <summary>
+        /// Prints Details of All the Employee
+        /// </summary>
+        /// <param name="employee">List of Employee</param>
+        void GetEmployeeDetails(List<IEmployee> employee);
+
+        /// <summary>
+        /// Prints the Employee Details under a Manager, Uses LINQ Query
+        /// </summary>
+        /// <param name="employee">List of Employee</param>
+        /// <param name="employeeId">ManagerId</param>
+        void GetSubOrdinatesByLINQ(List<IEmployee> employee, int employeeId);
+
+    }
+}
