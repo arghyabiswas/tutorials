d0be272be799346c84ce527456810b955eade38a
dRocks7 <53247908+dRocks7@users.noreply.github.com>
2019-07-30 18:24:56 +0530

    assignment_6

---
+++ Assignment/Assignment-6/Assignment-6/Assignment-6/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment_6.Startup))]
+namespace Assignment_6
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
