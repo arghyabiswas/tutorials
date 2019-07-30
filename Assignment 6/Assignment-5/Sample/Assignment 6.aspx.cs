db78a14be7f642b40b139c74ab5764b94cbb8188
dashashutosh80 <53247858+dashashutosh80@users.noreply.github.com>
2019-07-30 17:27:28 +0530

    commit

---
+++ Assignment 6/Assignment-5/Sample/Assignment 6.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment_5.Sample
+{
+    public partial class Assignment_6 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Extract the data stored in Session object to textbox
+            TextBox1.Text = Convert.ToString(Session["FirstName"]);
+            TextBox2.Text = Convert.ToString(Session["LastName"]);
+            TextBox3.Text = Convert.ToString(Session["Department"]);
+            TextBox4.Text = Convert.ToString(Session["Email"]);
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //Clicking Go Back button will cause redirection
+            Server.Transfer("Assignment-5.aspx");
+        }
+    }
+}
