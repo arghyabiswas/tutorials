3241043006d14e8d544a13331f477eafafe75be5
Aseem8984 <53247869+Aseem8984@users.noreply.github.com>
2019-08-02 11:37:08 +0530

    updated

---
+++ Day12/Assignment2/BusinessModel/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessModel
+{
+    public class Employee : IEmployee//Implimenting IEmployee(interface)
+
+    {
+
+        public Employee(int EmployeeId)//Constructor with parameter of type int 
+        {
+            this.EmployeeId = EmployeeId;
+        }
+        public int EmployeeId { get; }//restricted to be readonly outside the interface
+
+        public string Name { get; set; }
+        
+
+        public string Email { get; set; }
+        
+
+        IEmployee IEmployee.ReportingManager { get; set; }
+        
+    }
+}
