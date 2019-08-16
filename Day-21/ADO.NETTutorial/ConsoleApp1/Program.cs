a7130dee0a7207d4c91063988d30aab6d7a3a5c8
nbeura <52789961+nbeura@users.noreply.github.com>
2019-08-16 13:03:38 +0530

    Solution Completed for Assignment Day-21

---
+++ Day-21/ADO.NETTutorial/ConsoleApp1/Program.cs
@@ -0,0 +1,178 @@
+﻿using ADO.NETTutorial.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static string databaseConnectionString = ConfigurationManager.ConnectionStrings["TutorialConnection"].ConnectionString;
+
+        static void Main(string[] args)
+        {
+
+            bool isValid = TestConnection();
+            Console.WriteLine(isValid);
+
+            Project project = new Project();
+            // project.Id = 4;
+            project.Name = "C# OOP";
+            project.Description = "Test Description";
+            project.SartDate = DateTime.Now;
+            project.EndDate = DateTime.Now.AddDays(10);
+            project.IsActive = true;
+
+
+            /* Coded Area By Nikolash */
+
+
+            ProjectDataHandler dataHandler = new ProjectDataHandler();
+
+            DataTable dataTable = dataHandler.LoadAllProjects();
+
+            if (dataTable != null)
+            {
+                foreach (DataRow dataRow in dataTable.Rows)
+                {
+                    Console.WriteLine(dataRow["Id"] + " || " + dataRow["Name"] + " || " + dataRow["Description"] + " || " + dataRow["StartDate"] + " || " + dataRow["EndDate"] + " || " + dataRow["IsActive"]);
+                }
+            }
+
+
+            Console.WriteLine("\n\nEnter Project Id to Delete Record");
+
+            int inputId = 4;
+            bool resultFlag = false;
+
+            while (!resultFlag)
+            {
+                Console.WriteLine("\nOnly Counting Numbers are allowed");
+                resultFlag = int.TryParse(Console.ReadLine(), out inputId);
+            }
+
+            bool insertResult = dataHandler.Delete(inputId);
+
+            if(insertResult)
+            {
+                Console.WriteLine($"Your data is Deleted of Project Id {inputId}");
+
+            }
+            else
+            {
+                Console.WriteLine("Error in Deleting try with Correct Project Id");
+            }
+
+
+
+
+
+            //isValid = AddUpdateProject(project);
+
+            //Console.WriteLine(isValid);
+
+            //List<Project> projects = new List<Project>();
+            //projects.Where(a => a.IsActive == true).ToList();
+
+            Console.ReadKey();
+
+        }
+
+        public static bool TestConnection()
+        {
+            bool isValid = false;
+            SqlConnection oCnn = new SqlConnection();
+            try
+            {
+
+                oCnn.ConnectionString = databaseConnectionString;
+                oCnn.Open();
+                isValid = true;
+            }
+            catch
+            {
+                isValid = false;
+            }
+            finally
+            {
+                oCnn.Close();
+            }
+
+            return isValid;
+        }
+
+        public static bool AddUpdateProject(Project project)
+        {
+            bool isValid = false;
+
+
+            using (SqlConnection oCnn = new SqlConnection())
+            {
+                oCnn.ConnectionString = databaseConnectionString;
+                try
+                {
+                    oCnn.Open();
+
+                    using (SqlCommand command = new SqlCommand())
+                    {
+                        command.Connection = oCnn;
+
+                        command.CommandText = "AddUpdateProject";
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.Add("Id", SqlDbType.Int);
+                        command.Parameters["Id"].Value = project.Id;
+                        command.Parameters["Id"].Direction = ParameterDirection.InputOutput;
+
+                        command.Parameters.Add("Name", SqlDbType.VarChar, 255);
+                        command.Parameters["Name"].Value = project.Name;
+
+                        command.Parameters.Add("Description", SqlDbType.NVarChar, 1023);
+                        command.Parameters["Description"].Value = project.Description;
+
+                        command.Parameters.Add("StartDate", SqlDbType.DateTime);
+                        command.Parameters["StartDate"].Value = project.SartDate;
+
+                        command.Parameters.Add("EndDate", SqlDbType.DateTime);
+                        command.Parameters["EndDate"].Value = project.EndDate;
+
+                        command.Parameters.Add("IsActive", SqlDbType.Bit);
+                        command.Parameters["IsActive"].Value = project.IsActive;
+
+                        command.ExecuteNonQuery();
+
+                        int id = Convert.ToInt32(command.Parameters["Id"].Value);
+
+                        isValid = true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    isValid = false;
+                }
+            }
+
+            return isValid;
+        }
+    }
+
+    public class Project
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime SartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
